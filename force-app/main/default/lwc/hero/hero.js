e297aff549c0faee56440c47e867fad75ad959aa
aryansingh15 <aryansingh12498@gmail.com>
2024-06-24 10:28:45 +0530

    Initial Commit

---
+++ force-app/main/default/lwc/hero/hero.js
@@ -0,0 +1,8 @@
+import { LightningElement } from 'lwc';
+import Image_Url from '@salesforce/resourceUrl/Images';
+
+export default class Hero extends LightningElement {
+
+
+  heroImage = Image_Url + '/Images/hero1.jpg';
+}
